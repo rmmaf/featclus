1706a12d6d085caa8638541b41ab2229908216d8
Rodrigo Monteiro de Moraes de Arruda Falcão <rmmaf98@gmail.com>
2025-10-28 13:20:56 -0300

    Add EPS value for the DBSCAN

--- src/featclus/featclus.py
+++ src/featclus/featclus.py
@@ -35,14 +35,14 @@ class FeatureSelection:
         pd.DataFrame: A DataFrame with the importance of each feature sorted.
     """
 
-    def __init__(self, data: pd.DataFrame, shifts: List = [5, 10, 50], n_jobs: int = 1):
+    def __init__(self, data: pd.DataFrame, shifts: List = [5, 10, 50], n_jobs: int = 1, dbscan_eps: float = 0.25):
         self.data = data
         self.shifts = shifts
         self.model = Pipeline(
             steps=[
                 ("scaler", MinMaxScaler()),
                 ("pca", PCA(0.8)),
-                ("clustering", DBSCAN()),
+                ("clustering", DBSCAN(eps=dbscan_eps)),
             ]
         )
         self.columns = data.columns
